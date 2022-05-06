19fcc458aefaea4daf70e0ecd492730369bf2bf5
Sasha Czerniawski <sashacandoit@gmail.com>
2022-05-06 06:59:44 -0400

    environment setup and files added

---
+++ app.py
@@ -0,0 +1,15 @@
+from flask import Flask, render_template, redirect, session, flash
+from models import connect_db, db
+# from forms import 
+from sqlalchemy.exc import IntegrityError
+
+app = Flask(__name__)
+app.config["SQLALCHEMY_DATABASE_URI"] = "postgresql:///flask_feedback"
+app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
+app.config["SQLALCHEMY_ECHO"] = True
+app.config["SECRET_KEY"] = "abc123"
+app.config['DEBUG_TB_INTERCEPT_REDIRECTS'] = False
+
+
+connect_db(app)
+
