a6606d8ab5ec07f4098deb53bb492e7bd8ae739e
Sasha Czerniawski <sashacandoit@gmail.com>
2022-05-06 07:17:23 -0400

    User model added and base template setup

--- models.py
+++ models.py
@@ -11,3 +11,17 @@ def connect_db(app):
 
     db.app = app
     db.init_app(app)
+
+
+class User(db.Model):
+    __tablename__ = "users"
+
+    id = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    username = db.Column(db.String(20), nullable=False, unique=True)
+    password = db.Column(db.Text, nullable=False)
+    email = db.Column(db.String(50), nullable=False, unique=True)
+    first_name = db.Column(db.String(30), nullable=False)
+    last_name = db.Column(db.String(30), nullable=False)
+
+    def __repr__(self):
+        return f"<User {self.id} username={self.username} first_name={self.first_name} last_name={self.last_name} email={self.email}>"
